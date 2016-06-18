52ca2748616b13aad1d8efe98fca129cc6c22e53
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2016-06-18 17:34:14 +0200

    style(comments): junk removal

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = koa()
 const port = process.env.PORT || 8080
 
 app.use(serve('./client'))
-// $ GET /hello.txt
-// app.use(serve('test/fixtures'))
-// or use absolute paths
-//app.use(serve(__dirname + '/test/fixtures'))
 
 app.listen(port)
 console.log(`listening on port ${port}`)
